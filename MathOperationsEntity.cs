6694ba5a335b2b2e046fb0326d555f42ce729fa2
Subhash Mishra <mishra.subhash@gmail.com>
2022-05-07 19:54:23 +0530

    Added Entity class

---
+++ MathOperationsEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Straumann.Assignment
+{
+    public class MathOperationsEntity
+    {
+    }
+}
